dc27bc05fb183b070392dfe7b6b2029dcfe8ed2f
PCerutti <pablocerutti@deglia.xyz>
2025-10-15 11:27:52 -0300

    Update purchase_report.py

--- models/purchase_report.py
+++ models/purchase_report.py
@@ -6,9 +6,9 @@ class PurchaseReport(models.Model):
     qty_pending = fields.Float(
         string='Cantidad pendiente',
         readonly=True,
-        help='Cantidad ordenada menos cantidad recibida'
+        help='Cantidad ordenada menos cantidad recibida',
     )
 
     def _select_additional_fields(self, fields):
-        fields['qty_pending'] = "po_line.product_qty - po_line.qty_received"
+        fields['qty_pending'] = "(po_line.product_qty - po_line.qty_received)"
         return super()._select_additional_fields(fields)
