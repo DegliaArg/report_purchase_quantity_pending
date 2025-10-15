63e428ce0db1843ff3703fb56604ae70f9b3dc34
PCerutti <pablocerutti@deglia.xyz>
2025-10-15 09:26:44 -0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Report Purchase Quantity Pending',
+    'version': '1.0',
+    'depends': ['purchase'],
+    'author': 'PCerutti',
+    'category': 'Purchases',
+    'description': 'Agrega campo de cantidad pendiente al reporte de compras',
+    'data': [],
+    'installable': True,
+}
